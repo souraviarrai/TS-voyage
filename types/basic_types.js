2092cba6050f72b93ff03e190e7264ce69b9e889
Sourav Rai <souraviarrai@gmail.com>
2024-05-06 17:43:21 +0600

    first commit

---
+++ types/basic_types.js
@@ -0,0 +1,13 @@
+var isDone = true;
+// console.log(isDone)
+var decimal = 1.244;
+var integer = 234;
+//console.log(decimal, integer);
+var first_name = 'Sourav';
+var age = 24;
+// console.log(`${first_name} will be ${age + 1} by next year`)
+var salary = [300, 200, 500, 800];
+// console.log(salary)
+var array;
+array = [2, 3, 'sourav'];
+console.log(array[2]);
---
+++ types/basic_types.ts
@@ -0,0 +1,26 @@
+let isDone: boolean = true;
+// console.log(isDone)
+
+
+let decimal: number = 1.244;
+let integer: number = 234;
+
+//console.log(decimal, integer);
+
+
+let first_name: string = 'Sourav'
+let age: number = 24
+
+// console.log(`${first_name} will be ${age + 1} by next year`)
+
+
+let salary: Array<number> = [300, 200, 500, 800]
+// console.log(salary)
+
+
+let array: [number, number, string]
+
+array = [2,3,'sourav']
+
+console.log(array[2])
+
