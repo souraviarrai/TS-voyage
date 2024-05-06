6561edfb2bbeb33abe84fe7e303e17bf8f37d094
Sourav Rai <souraviarrai@gmail.com>
2024-05-06 18:46:32 +0600

    unknown datatypes

--- types/basic_types.js
+++ types/basic_types.js
@@ -10,4 +10,21 @@ var salary = [300, 200, 500, 800];
 // console.log(salary)
 var array;
 array = [2, 3, 'sourav'];
-console.log(array[2]);
+//console.log(array[2])
+// enums
+var Color;
+(function (Color) {
+    Color[Color["red"] = 1] = "red";
+    Color[Color["green"] = 2] = "green";
+    Color[Color["blue"] = 3] = "blue";
+})(Color || (Color = {}));
+var c = Color.red;
+// console.log(c)
+var Country;
+(function (Country) {
+    Country[Country["germany"] = 10] = "germany";
+    Country[Country["france"] = 20] = "france";
+    Country[Country["unitedKingdom"] = 30] = "unitedKingdom";
+})(Country || (Country = {}));
+var country = Country[30];
+console.log(country);
--- types/basic_types.ts
+++ types/basic_types.ts
@@ -22,5 +22,44 @@ let array: [number, number, string]
 
 array = [2,3,'sourav']
 
-console.log(array[2])
+//console.log(array[2])
+
+
+// enums
+
+
+enum Color {
+    red = 1,
+    green = 2,
+    blue = 3
+};
+
+let c: Color = Color.red;
+
+// console.log(c)
+
+
+enum Country {
+    germany = 10,
+    france = 20,
+    unitedKingdom = 30
+};
+
+let country: string = Country[30];
+// console.log(country)
+
+
+// unknown
+
+
+declare const mayBe: unknown;
+
+if (mayBe === true) {
+    const aBool: boolean = mayBe;
+}
+
+if (mayBe === 'string'){
+    const aString: string = mayBe
+}
+
 
